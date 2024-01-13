c90feb362d1510cbd4d6a8472536c81647664aa9
Ally Guo <69687800+ally-223@users.noreply.github.com>
2024-01-13 17:29:31 -0500

    recipecard component add

---
+++ components/RecipeCard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const RecipeCard = () => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Salad</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#D9D9D9', 
+    padding: 15,
+    borderRadius: 8,
+    opacity: 0.5,
+    width: 150,
+    height: 103,
+    flexShrink: 0, 
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default RecipeCard;
--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,15 +1,23 @@
 // Import necessary components and modules from React Native
 import React from 'react';
+import  RecipeCard from "../components/RecipeCard"
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 
 const HomePage = () => {
     // Use return to render JSX
     return (
-       
         <View>
+
+            <View>
             <Text style={styles.heading}>Welcome Back</Text>
             <Text style={styles.heading}>Remake your past recipes or generate new ones</Text>
+            </View>
+
+            <View>
+                <RecipeCard/>
+            </View>
         </View>
+        
     );
 }
 
