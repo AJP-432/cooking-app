ebde4b574c7123dbf4d1118768918f3d7b9dacf5
Ajlal Paracha <ajlal.paracha@gmail.com>
2024-01-13 16:25:25 -0500

    Initial userpage

---
+++ pages/UserPage.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+function Profile(pic_image = '../assets/gordon-ramsay.jpg') {
+    return (
+    <View>
+        <Image />
+    </View>
+}
+
+export default function UserPage() {
+    return <Text>Hello World!</Text>;
+}
